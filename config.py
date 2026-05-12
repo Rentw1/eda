8693f8efdafb7f6e07a9719e2e063bf32bd2f236
Rentw1 <116452403+Rentw1@users.noreply.github.com>
2026-05-12 22:08:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN env variable is not set")
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY env variable is not set")
