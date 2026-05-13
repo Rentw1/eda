5b0bd825b92ec91089bc1b07a68be18ae1b5a0c9
Rentw1 <116452403+Rentw1@users.noreply.github.com>
2026-05-13 19:41:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-GIGACHAT_CREDENTIALS = os.getenv("GIGACHAT_CREDENTIALS", "")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN env variable is not set")
-if not GIGACHAT_CREDENTIALS:
-    raise ValueError("GIGACHAT_CREDENTIALS env variable is not set")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+GIGACHAT_CREDENTIALS = os.environ.get("GIGACHAT_CREDENTIALS", "")
